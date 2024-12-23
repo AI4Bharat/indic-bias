9402b79911122746ed72ce05df62de4f4c6d216b
vignesh191003 <125024872+vignesh191003@users.noreply.github.com>
2024-12-23 16:53:17 +0530

    with question_helpers.py

--- firebase/question_helpers.py
+++ firebase/question_helpers.py
@@ -1,1 +1,12 @@
-from firebase import
+from firebase import db
+
+questions_ref = db.collection('questions')
+
+
+def get_questions():
+    result = []
+    for question in questions_ref.stream():
+        question = question.to_dict() | {'id': question.id}
+        result.append(question)
+
+    return result
