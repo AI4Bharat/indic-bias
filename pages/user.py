63638d247495590a6a34dabf5e85171d2046c95c
vignesh191003 <125024872+vignesh191003@users.noreply.github.com>
2024-12-23 16:19:04 +0530

    Added requirements.txt

--- pages/user.py
+++ pages/user.py
@@ -1,4 +1,5 @@
 import streamlit as st
+
 from firebase import allowed_emails
 
 if "userObj" not in st.session_state:
@@ -10,7 +11,4 @@ if userObj.get("email") not in allowed_emails:
     st.session_state.error = {'message': 'You are not allowed to access this app.'}
     st.switch_page('main.py')
 
-
 st.markdown(f"## **Welcome {userObj['displayName']}**")
-
-st.markdown('TEST')
