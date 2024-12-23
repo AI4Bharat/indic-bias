faeafb86b0ef05db2a514564ac37820fbbb98759
vignesh191003 <125024872+vignesh191003@users.noreply.github.com>
2024-12-23 17:34:08 +0530

    init

--- firebase/__init__.py
+++ firebase/__init__.py
@@ -14,14 +14,15 @@ db = firestore.client(app)
 web_api_key = st.secrets.firebaseAPIKEY.key
 
 oauth_client = GoogleOAuth2(client_id=st.secrets.oAuth.client_id, client_secret=st.secrets.oAuth.client_secret)
+master_ref = db.collection('master')
+
 emails_ref = db.collection('emails')
+statements_ref = db.collection('statements')
 
 allowed_emails = [doc.id for doc in emails_ref.stream()]
 
 # DB INIT SCRIPT
 '''
-emails_ref = db.collection('emails')
-questions_ref = db.collection('questions')
 
 admin_emails = ['janani23ai@gmail.com', 'jsvigneshbabu83@gmail.com']
 user_emails = ['21f3002138@ds.study.iitm.ac.in', '22f1001225@ds.study.iitm.ac.in']
--- firebase/question_helpers.py
+++ firebase/question_helpers.py
@@ -1,11 +1,22 @@
+from google.cloud.firestore_v1.base_query import FieldFilter
+
 from firebase import db
 
-questions_ref = db.collection('questions')
+statements_ref = db.collection('statements')
 
 
-def get_questions():
+def get_all_statements():
     result = []
-    for question in questions_ref.stream():
+    for question in statements_ref.stream():
+        question = question.to_dict() | {'id': question.id}
+        result.append(question)
+
+    return result
+
+
+def get_statements_by_type(axes, type):
+    result = []
+    for question in statements_ref.where(filter=FieldFilter('axes', '==', axes)).where('type', '==', type).stream():
         question = question.to_dict() | {'id': question.id}
         result.append(question)
 
