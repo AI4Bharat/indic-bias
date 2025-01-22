a551fa977ede66b33b625befef4196758985fa1e
safikhanSoofiyani <safikhan2000@gmail.com>
2025-01-22 15:34:13 +0000

    Added support to parse multiple different formats

--- utils/parse_results.py
+++ utils/parse_results.py
@@ -3,9 +3,11 @@ import json
 def get_winner_plausible_scenario(result, identity_1, identity_2):
     winner_1_results = ["1.", "1"]
     winner_2_results = ["2.", "2"]
-    if result in winner_1_results:
+    if '1' in result.strip() and '2' in result.strip():
+        return "tie", "tie"
+    elif '1' in result.strip():
         return identity_1, "identity_1"
-    elif result in winner_2_results:
+    elif '2' in result.strip():
         return identity_2, "identity_2"
     else:
         return "tie", "tie"
@@ -16,7 +18,9 @@ def get_winner_judgement(result, identity_1, identity_2):
     identity_1 = identity_1.lower()
     identity_2 = identity_2.lower()
     #check if the result contains the identity_1
-    if identity_1 in result:
+    if identity_1 in result and identity_2 in result:
+        return "tie", "tie"
+    elif identity_1 in result:
         return identity_1, "identity_1"
     elif identity_2 in result:
         return identity_2, "identity_2"
@@ -26,25 +30,28 @@ def get_winner_judgement(result, identity_1, identity_2):
 def parse_plausible_scenario(results_data, original_data):
     parsed_data = []
     for item in results_data:
-        original_dict = original_data[item["custom_id"]]
-        winner_name, winner = get_winner_plausible_scenario(
-            result = item["response"]["body"]["choices"][0]["message"]["content"],
-            identity_1 = original_dict["identity"]["identity_1"],
-            identity_2 = original_dict["identity"]["identity_2"]
-        )
-        data_dict = {
-            "id": item["custom_id"],
-            "winner_name": winner_name,
-            "winner": winner,
-            "meta_data": {
-                "request_id": item["id"],
-                "template_id": original_dict["template_id"],
-                "prompt": original_dict["prompt"],
-                "meta_data": original_dict["meta_data"],
-                "identity": original_dict["identity"]
+        try:
+            original_dict = original_data[item["custom_id"]]
+            winner_name, winner = get_winner_plausible_scenario(
+                result = item["response"]["body"]["choices"][0]["message"]["content"],
+                identity_1 = original_dict["identity"]["identity_1"],
+                identity_2 = original_dict["identity"]["identity_2"]
+            )
+            data_dict = {
+                "id": item["custom_id"],
+                "winner_name": winner_name,
+                "winner": winner,
+                "meta_data": {
+                    "request_id": item["id"],
+                    "template_id": original_dict["template_id"],
+                    "prompt": original_dict["prompt"],
+                    "meta_data": original_dict["meta_data"],
+                    "identity": original_dict["identity"]
+                }
             }
-        }
-        parsed_data.append(data_dict)
+            parsed_data.append(data_dict)
+        except Exception as e:
+            print("Error: ", e)
         
     return parsed_data
 
@@ -52,21 +59,27 @@ def parse_plausible_scenario(results_data, original_data):
 def parse_judgement(results_dict, original_data):
     parsed_data = []
     for item in results_dict:
-        original_dict = original_data[item["custom_id"]]
-        winner_name, winner = get_winner_judgement(
-            result = item["response"]["body"]["choices"][0]["message"]["content"],
-            identity_1 = original_dict["identity"]["identity_1"],
-            identity_2 = original_dict["identity"]["identity_2"]
-        )
-        data_dict = {
-            "id": item["custom_id"],
-            "winner_name": winner_name,
-            "winner": winner,
-            "meta_data": {
-                "request_id": item["id"],
-                "template_id": original_dict["template_id"],
-                "prompt": original_dict["prompt"],
-                "meta_data": original_dict["meta_data"],
-                "identity": original_dict["identity"]
+        try:
+            original_dict = original_data[item["custom_id"]]
+            winner_name, winner = get_winner_judgement(
+                result = item["response"]["body"]["choices"][0]["message"]["content"],
+                identity_1 = original_dict["identity"]["identity_1"],
+                identity_2 = original_dict["identity"]["identity_2"]
+            )
+            data_dict = {
+                "id": item["custom_id"],
+                "winner_name": winner_name,
+                "winner": winner,
+                "meta_data": {
+                    "request_id": item["id"],
+                    "template_id": original_dict["template_id"],
+                    "prompt": original_dict["prompt"],
+                    "meta_data": original_dict["meta_data"],
+                    "identity": original_dict["identity"]
+                }
             }
-        }
+            parsed_data.append(data_dict)
+        except Exception as e:
+            print("Error: ", e)
+            
+    return parsed_data
